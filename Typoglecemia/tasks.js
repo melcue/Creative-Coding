22b7adaf7b7c2ee0e4edba31ddc574215e176014
melanie c <107213962+melcue@users.noreply.github.com>
2022-08-29 18:25:59 -0500

    Create tasks.js

---
+++ Typoglecemia/tasks.js
@@ -0,0 +1,68 @@
+
+const tasks = [
+  {
+    sentence: "Chicagoans forward their clocks by one hour during spring which is referred to as daylight savings time.",
+    question: "How many hours ahead are Chicago's clocks in spring?",
+    answers: ["Two", "One"],
+    correctAnswer: 1,
+  },
+  {
+    sentence: "Thailand is know as the land of white elephant because the thai royal family owns elephants with lighter skin color.",
+    question: "What is another name for Thailand?",
+    answers: ["Land of grey elephant", "Land of white elephant"],
+    correctAnswer: 1,
+  },
+  {
+    sentence:
+      "The kilobyte is recognized as being the smallest unit of memory while the largest unit of memory is the yottabyte.",
+    question: "Which unit of memory is the smallest",
+    answers: ["Kilobyte", "Byte"],
+    correctAnswer: 0,
+  },
+  {
+    sentence:
+      "Researchers discovered that classical music helps plants grow better and faster.",
+    question: "Does electronic music help plants grow better and faster?",
+    answers: ["Yes", "No"],
+    correctAnswer: 1,
+  },
+  {
+    sentence: "Diamonds are the hardest rocks found in the world while talc is the softest rock in the world.",
+    question: "Is talc the hardest rock found in the word?",
+    answers: ["True", "False"],
+    correctAnswer: 1,
+  },
+  {
+    sentence:
+      "Our nails, hair strands and outer layer of the skin are made of a substance called keratin.",
+    question: "Which substance makes the outer of the skin?",
+    answers: ["Keratin", "Cells"],
+    correctAnswer: 0,
+  },
+  {
+    sentence:
+      "England used eels as currency for rental agreements in medieval times.",
+    question:
+      "What currency was used in medieval England in rental agreements?",
+    answers: ["Eels", "Guilders"],
+    correctAnswer: 0,
+  },
+  {
+    sentence: "Poland is the original creator of bagels.",
+    question: "Where did bagels orignate from?",
+    answers: ["Holland", "Poland"],
+    correctAnswer: 1,
+  },
+  {
+    sentence: "A seismologist studies earthquakes and geological materials.",
+    question: "People who study earthquakes are called?",
+    answers: ["Seismologists", "Qualogists"],
+    correctAnswer: 0,
+  },
+  {
+    sentence: "Morroco has a red flag with the greenest star ever in the middle",
+    question: "Does Morroco have a red flag with a blue star in the middle?",
+    answers: ["True", "False"],
+    correctAnswer: 1,
+  },
+];
